3d5a9c36c44788faaa546da3f847683dd306fd89
1305abhi <86284228+1305abhi@users.noreply.github.com>
2024-09-30 21:44:55 +0530

    Update supabase.js

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,9 +1,12 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key must be provided in environment variables.");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
-        
+export default supabase;
